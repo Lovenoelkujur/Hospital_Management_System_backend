14d46de248f1a00b24e57d1a44edf995018171df
Noel Kujur <lovenoelkujur@gmail.com>
2024-06-15 13:14:06 +0530

    old cors

--- app.js
+++ app.js
@@ -17,33 +17,33 @@ dotenv.config({
 });
 
 // Link Frontend
-// app.use(
-//     cors({
-//         origin : [process.env.FRONTEND_URL, process.env.DASHBOARD_URL],
-//         methods : ["GET", "POST", "PUT", "DELETE"],
-//         credentials : true,
-//     })
-// );
+app.use(
+    cors({
+        origin : [process.env.FRONTEND_URL, process.env.DASHBOARD_URL],
+        methods : ["GET", "POST", "PUT", "DELETE"],
+        credentials : true,
+    })
+);
 
-const whitelist = [process.env.FRONTEND_URL, process.env.DASHBOARD_URL]
-const corsOptions = {
-  origin : function (origin, callback) {
-    // if (whitelist.indexOf(origin) !== -1) {
-    //   callback(null, true)
-    // } else {
-    //   callback(new Error('Not allowed by CORS'))
-    // }
-    if (whitelist.indexOf(req.header('Origin')) !== -1) {
-        corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
-      } else {
-        corsOptions = { origin: false } // disable CORS for this request
-      }
-  },
-  methods : ["GET", "POST", "PUT", "DELETE"],
-//   credentials : true,
-}
+// const whitelist = [process.env.FRONTEND_URL, process.env.DASHBOARD_URL]
+// const corsOptions = {
+//   origin : function (origin, callback) {
+//     // if (whitelist.indexOf(origin) !== -1) {
+//     //   callback(null, true)
+//     // } else {
+//     //   callback(new Error('Not allowed by CORS'))
+//     // }
+//     if (whitelist.indexOf(req.header('Origin')) !== -1) {
+//         corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+//       } else {
+//         corsOptions = { origin: false } // disable CORS for this request
+//       }
+//   },
+//   methods : ["GET", "POST", "PUT", "DELETE"],
+// //   credentials : true,
+// }
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 
 app.use(cookieParser());    // To pass Cookie
 app.use(express.json());    // To pass json data
